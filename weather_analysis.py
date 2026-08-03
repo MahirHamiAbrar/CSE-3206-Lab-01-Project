84578bd379392cd23c11ac232770ee5117dd0711
R-Mahin <riduanislam425@gmail.com>
2026-08-04 03:31:44 +0600

    exprimenting again

--- weather_analysis.py
+++ weather_analysis.py
@@ -13,6 +13,7 @@ def plot_temperature_trends(data, save_path="temperature_trends.png"):
     pass
 
 
+
 def plot_precipitation_and_humidity(data, save_path="precip_humidity.png"):
     """ Plot daily precipitation (bar) and mean humidity (line, secondary axis). """
     daily = data["daily"]
@@ -46,6 +47,9 @@ def main():
     data = load_weather_data(filepath)
     plot_temperature_trends(data)
     plot_precipitation_and_humidity(data)
+
+def hello():
+    print("Experimenting")
 
 
 if __name__ == "__main__":
